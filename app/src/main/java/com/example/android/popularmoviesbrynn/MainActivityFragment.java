1d4d71ded960d38782c89a479d7e279715bde5e6
brynn-udacity <brynn@udacity.com>
2015-09-29 09:18:02 -0700

    created a gridview

--- app/src/main/java/com/example/android/popularmoviesbrynn/MainActivityFragment.java
+++ app/src/main/java/com/example/android/popularmoviesbrynn/MainActivityFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    private final String LOG_TAG = MainActivityFragment.class.getSimpleName();
 
     public MainActivityFragment() {
     }
