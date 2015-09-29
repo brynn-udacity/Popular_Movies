fb563a5c36ad50957ff75c91fcb2eaf7b3b300b9
brynn-udacity <brynn@udacity.com>
2015-09-29 10:23:35 -0700

    imageadapter added, not working

--- app/src/main/java/com/example/android/popularmoviesbrynn/MainActivityFragment.java
+++ app/src/main/java/com/example/android/popularmoviesbrynn/MainActivityFragment.java
@@ -1,10 +1,20 @@
 package com.example.android.popularmoviesbrynn;
 
-import android.support.v4.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -12,12 +22,62 @@ import android.view.ViewGroup;
 public class MainActivityFragment extends Fragment {
     private final String LOG_TAG = MainActivityFragment.class.getSimpleName();
 
-    public MainActivityFragment() {
-    }
+
+
+    static final List<String> postersList = Arrays.asList(
+            "http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg",
+            "http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg",
+            "http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg",
+            "http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg",
+            "http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg",
+            "http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg",
+            "http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg",
+            "http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg",
+            "http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg",
+            "http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg");
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridView1);
+
+        ImageAdapter adapter = new ImageAdapter(getActivity(),
+                android.R.layout.simple_list_item_1, postersList);
+
+        gridView.setAdapter(adapter);
+
+        return rootView;
+    }
+
+
+    private class ImageAdapter extends ArrayAdapter<String>{
+        private List<String> imageList;
+        Context mContext;
+
+        public ImageAdapter(Context context, int resource, List<String> objects){
+            super(context, resource, objects);
+            mContext = context;
+            imageList = objects;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            ImageView imageView;
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(8, 8, 8, 8);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+            Log.e(LOG_TAG, Integer.toString(position));
+            Picasso.with(mContext).load(imageList.get(position)).into(imageView);
+            return imageView;
+        }
     }
 }
